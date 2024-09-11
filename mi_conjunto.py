48e58028d7f896cfcaed9e6e2c54486972593112
Luis Herrera <a.22308051281207@cbtis128.edu.mx>
2024-09-11 07:44:26 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,7 @@
+print("--ejemplo de conjuntos")
+tipodeportes={"Futbol","Tenis","Basketbol"}
+print(tipodeportes)
+print("--Lista de elementos del conjunto tipo deportes loop for--")
+print(tipodeportes)
+for herrera in tipodeportes:
+    print (herrera)
--- mi_tuplas.py
+++ mi_tuplas.py
@@ -1,3 +1,11 @@
 print("--Ejemplo de tuplas--")
 canciones=("te amo","El NoNoa","amor eterno")
 print(canciones)
+y = list(canciones)
+print(y)
+y[1] = "La puerta negra"
+x = tuple(y)
+print(x)
+print("--Listado de elementos de la tupla canciones con ciclo for--")
+for herrera in canciones:
+    print(herrera)
